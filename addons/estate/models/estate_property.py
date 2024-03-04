39104c845326310ac3a64b9dfc127acd9cda411d
Meriana <merianaalchami@gmail.com>
2024-03-04 16:14:44 +0100

    add some functions (estate_property_offer/estate_property)
    add method onchange()
    add cumpute and inverse methods

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -1,9 +1,10 @@
-from odoo import models, fields
+from odoo import models, fields, api
 from datetime import datetime, timedelta
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Real Estate Property'
+
 
     name = fields.Char(string='Name', required=True, default = 'My new house')
     description = fields.Text(string='Description')
@@ -13,10 +14,28 @@ class EstateProperty(models.Model):
     selling_price = fields.Float(string='Selling Price', readonly= True)
     bedrooms = fields.Integer(string='Bedrooms' , default = 2)
     living_area = fields.Integer(string='Living Area')
+    garden_area = fields.Integer(string='Garden Area')
+    total_area = fields.Integer(string="Total Area", compute='_compute_total_area')
+    best_price = fields.Float(string='Best Price', compute='_compute_best_price', store=True)
+
+    """ champ total area """
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for property in self:
+            property.total_area = property.living_area + property.garden_area
+            
+    """ champ best offer """
+    @api.depends('property_offer_ids.price')
+    def _compute_best_price(self):
+        for property_record in self:
+            if property_record.property_offer_ids:
+                property_record.best_price = max(property_record.property_offer_ids.mapped('price'))
+            else:
+                property_record.best_price = 0.0
+            
     facades = fields.Integer(string='Facades')
     garage = fields.Boolean(string='Garage')
     garden = fields.Boolean(string='Garden')
-    garden_area = fields.Integer(string='Garden Area')
     garden_orientation = fields.Selection([
         ('north', 'North'),
         ('south', 'South'),
@@ -24,6 +43,17 @@ class EstateProperty(models.Model):
         ('west', 'West')
     ], string='Garden Orientation', default= 'north')
 
+    """ méthod apappelée lorsqu'il y a un changement dans le champ "garden" """
+    
+    @api.onchange('garden')
+    def onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation= 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation=  False
+        
 
     last_seen = fields.Datetime("Last Seen", default=fields.Datetime.now)
     active = fields.Boolean(string='Active', default=False)
--- addons/estate/models/estate_property_offer.py
+++ addons/estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import timedelta
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -8,3 +9,16 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection([('accepted', 'Accepted'), ('refused', 'Refused')], string='Status', required=True, copy=False)
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
     property_id = fields.Many2one('estate.property', string='Property', required=True)
+    validity = fields.Integer(string='Validity (days)', default=7)
+    date_deadline = fields.Date(string='Deadline', compute= '_compute_date_deadline', inverse='_set_date_from_deadline')
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for offer in self:
+            if offer.create_date:
+                offer.date_deadline = offer.create_date + timedelta(days=offer.validity)
+
+    def _set_date_from_deadline(self):
+        for offer in self:
+            if offer.date_deadline and offer.create_date:
+                offer.validity = (offer.date_deadline - offer.create_date).days
